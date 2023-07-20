8323b92f403abf4116274146071db6f5667404db
Charles Mwaniki <mwanikigachanja@gmail.com>
2023-07-20 09:18:49 -0700

    Nested Limit

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,39 +1,22 @@
 #include "main.h"
-#include <limits.h>
 
 /**
- * print_number - Prints an integer using _putchar
- * @n: The integer to be printed
+ * print_number - Prints a number
+ * @n: The number to print
  */
+
 void print_number(int n)
 {
-    int is_negative = 0;
+	unsigned int num = n;
 
-    if (n == 0)
-    {
-        _putchar('0');
-        return;
-    }
-
-    if (n < 0)
-    {
-        if (n == INT_MIN)
-        {
-            is_negative = 1;
-            n++;
-        }
-        else
-        {
-            _putchar('-');
-            n = -n;
-        }
-    }
-
-    if (n / 10 != 0)
-        print_number(n / 10);
-
-    _putchar((n % 10) + '0');
-
-    if (is_negative)
-        _putchar('8'); 
+	if (n < 0)
+	{
+		_putchar('-');
+		num = -num;
+	}
+	if (num > 9)
+	{
+		print_number(num / 10);
+	}
+	_putchar(num % 10 + '0');
 }
