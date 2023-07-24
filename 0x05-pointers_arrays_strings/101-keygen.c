f41ed5524266662ffecd84a980f702e92ddf0f71
Charles Mwaniki <mwanikigachanja@gmail.com>
2023-07-24 01:58:50 -0700

    Keygenerator

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,33 @@
+#include <time.h>
+#include <stdlib.h>
+#include <stdio.h>
+#define stopLimit 2645
+#define asciiStop 127
+#define asciiStart 32
+
+/**
+ * main - generates a random password
+ * Return: 0 if success
+ */
+
+int main(void)
+{
+	char password[100];
+	int randValue, num = 0, i = 0;
+
+	srand(time(NULL));
+
+	while (num < stopLimit)
+	{
+		randValue = random() % asciiStop;
+		if (randValue > asciiStart)
+		{
+			password[i++] = randValue;
+			num += randValue;
+		}
+	}
+	password[i++] = (2772 - num);
+	password[i] = '\0';
+	printf("%s", password);
+	return (0);
+}
