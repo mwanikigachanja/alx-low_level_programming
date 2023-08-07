f43968af55028777b08ab637ec61f589c0a55b7f
Charles Mwaniki <mwanikigachanja@gmail.com>
2023-08-07 01:53:20 -0700

    String to word

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,3 +1,25 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * ch_free_grid - frees a 2 dimensional array.
+ * @grid: multidimensional array of char.
+ * @height: height of the array.
+ *
+ * Return: no return
+ */
+
+void ch_free_grid(char **grid, unsigned int height)
+{
+	if (grid != NULL && height != 0)
+	{
+		for (; height > 0; height--)
+			free(grid[height]);
+		free(grid[height]);
+		free(grid);
+	}
+}
+
 /**
  * strtow - splits a string into words.
  * @str: string.
