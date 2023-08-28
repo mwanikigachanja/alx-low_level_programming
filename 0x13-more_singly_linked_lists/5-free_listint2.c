61df08effbde96ac18eb818088e77922b00d8c8f
Charles Mwaniki <mwanikigachanja@gmail.com>
2023-08-28 08:14:13 -0700

    Free linked list

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * free_listint2 - a function ...
+ * @head: the list
+ *
+ * Return: 1 or 0
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *ptr, *tmp;
+
+	if (head == NULL)
+		return;
+	ptr = *head;
+	while (ptr != NULL)
+	{
+		tmp =  ptr;
+		ptr = ptr->next;
+		free(tmp);
+	}
+	*head = NULL;
+}
