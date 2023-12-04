c2adf94b5d730686a852cf3cff2077043cec1689
Mwaniki Charles <mwanikigachanja@gmail.com>
2023-12-03 22:29:06 -0800

    retrieves a value associated with a key.

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -8,5 +8,19 @@
  */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
+hash_node_t *node;
+unsigned long int index;
 
+if (ht == NULL || key == NULL)
+return (NULL);
+
+index = key_index((const unsigned char *)key, ht->size);
+node = ht->array[index];
+while (node)
+{
+if (strcmp(node->key, key) == 0)
+return (node->value);
+node = node->next;
+}
+return (NULL);
 }
