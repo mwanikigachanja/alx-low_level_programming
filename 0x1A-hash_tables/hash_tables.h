b07b232309eaf3678999f487e96f203c9f20d705
Mwaniki Charles <mwanikigachanja@gmail.com>
2023-12-03 22:55:31 -0800

    Sorted hash tables

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -7,7 +7,6 @@
 
 /**
  * struct hash_node_s - Node of a hash table
- *
  * @key: The key, string
  * The key is unique in the HashTable
  * @value: The value corresponding to a key
@@ -22,7 +21,6 @@ typedef struct hash_node_s
 
 /**
  * struct hash_table_s - Hash table data structure
- *
  * @size: The size of the array
  * @array: An array of size @size
  * Each cell of this array is a pointer to the first node of a linked list,
@@ -34,9 +32,16 @@ typedef struct hash_table_s
 	hash_node_t **array;
 } hash_table_t;
 
+hash_table_t *hash_table_create(unsigned long int size);
+unsigned long int hash_djb2(const unsigned char *str);
+unsigned long int key_index(const unsigned char *key, unsigned long int size);
+int hash_table_set(hash_table_t *ht, const char *key, const char *value);
+char *hash_table_get(const hash_table_t *ht, const char *key);
+void hash_table_print(const hash_table_t *ht);
+void hash_table_delete(hash_table_t *ht);
+
 /**
  * struct shash_node_s - Node of a sorted hash table
- *
  * @key: The key, string
  * The key is unique in the HashTable
  * @value: The value corresponding to a key
@@ -55,7 +60,6 @@ typedef struct shash_node_s
 
 /**
  * struct shash_table_s - Sorted hash table data structure
- *
  * @size: The size of the array
  * @array: An array of size @size
  * Each cell of this array is a pointer to the first node of a linked list,
@@ -71,18 +75,11 @@ typedef struct shash_table_s
 	shash_node_t *stail;
 } shash_table_t;
 
-hash_table_t *hash_table_create(unsigned long int size);
-unsigned long int hash_djb2(const unsigned char *str);
-unsigned long int key_index(const unsigned char *key, unsigned long int size);
-int hash_table_set(hash_table_t *ht, const char *key, const char *value);
-char *hash_table_get(const hash_table_t *ht, const char *key);
-void hash_table_print(const hash_table_t *ht);
-void hash_table_delete(hash_table_t *ht);
-
 shash_table_t *shash_table_create(unsigned long int size);
 int shash_table_set(shash_table_t *ht, const char *key, const char *value);
 char *shash_table_get(const shash_table_t *ht, const char *key);
 void shash_table_print(const shash_table_t *ht);
 void shash_table_print_rev(const shash_table_t *ht);
 void shash_table_delete(shash_table_t *ht);
+
 #endif /* HASH_TABLES_H */
